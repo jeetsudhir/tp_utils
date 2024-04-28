69752a2840fb23af6c665041fb5f0ae1e2436772
jeetsudhir <asudhir@umich.edu>
2024-04-27 21:31:51 -0400

    Changes to code

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,44 @@
 use std::env;
 use std::fs;
 use std::io;
-use std::path::{PathBuf, Path};
+use std::path::PathBuf;
 use structopt::StructOpt;
 use std::process::{Command, Stdio};
 
 #[derive(StructOpt, Debug)]
-struct Opt {
+#[structopt(name = "tp_utils", about = "A teleport command for the terminal.")]
+enum Opt {
     /// Set a new teleport point
-    #[structopt(short, long)]
-    set: bool,
-
-    /// The name of the teleport point
-    name: Option<String>,
+    #[structopt(name = "set")]
+    Set {
+        /// The name of the teleport point
+        name: String,
+    },
 
     /// List all teleport points
-    #[structopt(long)]
-    list: bool,
+    #[structopt(name = "list")]
+    List,
 
-    /// Remove a teleport point
-    #[structopt(long)]
-    delete: bool,
+    /// Remove a teleport point or all teleport points
+    #[structopt(name = "delete")]
+    Delete {
+        /// The name of the teleport point to delete
+        name: Option<String>,
 
-    /// Remove all teleport points
-    #[structopt(long)]
-    all: bool,
+        /// Remove all teleport points
+        #[structopt(short = "a", long = "all")]
+        all: bool,
+    },
+
+    /// Teleport to a point
+    #[structopt(name = "tp")]
+    Teleport {
+        /// The name of the teleport point
+        name: String,
+    },
 }
+
+
 
 fn get_tp_dir() -> PathBuf {
     dirs::home_dir().unwrap().join(".tp")
@@ -47,8 +60,10 @@ fn set_tp_point(name: &str) -> io::Result<()> {
             let existing_name = entry.file_name().into_string().unwrap();
             return Err(io::Error::new(io::ErrorKind::AlreadyExists, format!("Teleport point to '{}' already exists with the name '{}'", current_dir, existing_name)));
         }
-    }
-    fs::write(tp_path, current_dir)
+    }    
+    fs::write(&tp_path, current_dir.clone())?;
+    println!("Teleport point '{}' set to '{}'", name, current_dir);
+    Ok(())
 }
 
 fn get_tp_point(name: &str) -> io::Result<()> {
@@ -90,7 +105,7 @@ fn get_tp_point(name: &str) -> io::Result<()> {
 fn list_tp_points() -> io::Result<()> {
     let tp_dir = get_tp_dir();
     if !tp_dir.exists() {
-        println!("No teleport points have been made. You can make a teleport point with the following command:\n\ncargo run -- --set --name <name>");
+        println!("No teleport points have been made. You can make a teleport point with the following command:\n\ntp set \"<name>\"");
         return Ok(());
     }
     for entry in fs::read_dir(tp_dir)? {
@@ -109,7 +124,8 @@ fn delete_tp_point(name: Option<String>, all: bool) -> io::Result<()> {
     } else {
         match name {
             Some(name) => {
-                let tp_path = tp_dir.join(name);
+                let tp_path = tp_dir.join(name.clone());
+                println!("Deleting teleport point '{}'", name);
                 fs::remove_file(tp_path)?;
             }
             None => eprintln!("You must provide a name for the teleport point to delete"),
@@ -120,23 +136,11 @@ fn delete_tp_point(name: Option<String>, all: bool) -> io::Result<()> {
 
 fn main() -> io::Result<()> {
     let opt = Opt::from_args();
-
-    if opt.list {
-        list_tp_points()?;
-    } else if opt.delete {
-        delete_tp_point(opt.name, opt.all)?;
-    } else {
-        match opt.name {
-            Some(name) => {
-                if opt.set {
-                    set_tp_point(&name)?;
-                } else {
-                    get_tp_point(&name)?;
-                }
-            }
-            None => eprintln!("You must provide a name for the teleport point"),
-        }
+    match opt {
+        Opt::Set { name } => set_tp_point(&name.trim())?,
+        Opt::List => list_tp_points()?,
+        Opt::Delete { name, all } => delete_tp_point(name.map(|n| n.trim().to_string()), all)?,
+        Opt::Teleport { name } => get_tp_point(&name.trim())?,
     }
-
     Ok(())
 }
